de970e5e619b1434ad7aab891a826772bdebf567
Daniel J. Gomez <djgomez@stanford.edu>
2024-12-24 21:01:44 -0800

    first commit

---
+++ exerkinemap/exerkinemap.py
@@ -0,0 +1,16 @@
+# Single-cell and Spatial Cell-cell communication frameworks  
+# @daniel1gomez
+import pandas as pd
+import scanpy as sc
+
+# Load CSV Data
+df = pd.read_csv('path_to_your/exerkinextissue.csv')
+
+# Assuming `df` is loaded from your CSV
+adata = sc.AnnData(
+    X=df[['Effect']].values,  # The main data matrix can be a placeholder, or any relevant numeric value
+    obs=df[['Exerkine', 'Source_Tissue', 'Target_Tissue', 'Biological_System']],
+    var=pd.DataFrame(index=df['Exerkine'])
+)
+
+adata.write_h5ad('path_to_save/exerkinextissue.h5ad')
