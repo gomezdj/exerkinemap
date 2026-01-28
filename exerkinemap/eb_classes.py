e58e4199b1a7f2164ed05552c4f6d2d33e57d452
Daniel J. Gomez <sfdanielgomez@gmail.com>
2026-01-28 04:23:37 -0800

    Create eb_classes.py

---
+++ exerkinemap/eb_classes.py
@@ -0,0 +1,12 @@
+EXERKINEMAP_CLASSES = {
+    0: "Sedentary_Baseline",
+    1: "Acute_Endurance_Response",      # IL-6, IL-15 signaling
+    2: "Acute_Resistance_Response",     # IGF-1, FGF2 pathways
+    3: "Chronic_Aerobic_Adaptation",    # VEGF, BDNF upregulation
+    4: "Chronic_Strength_Adaptation",   # Myokine/hepatokine crosstalk
+    5: "Recovery_Phase",                # Anti-inflammatory, tissue repair
+    6: "Metabolic_Stress",              # Lactate, ROS signaling
+    7: "Immune_Activation",             # Cytokine storm, inflammation
+    8: "Angiogenic_Remodeling",         # Endothelial-muscle crosstalk
+    9: "Neuromuscular_Plasticity"       # BDNF, neurotrophin signaling
+}
